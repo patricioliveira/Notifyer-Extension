df900b554f7372e29f7ec8156da236c273a18058
Patricio Oliveira - DEV202 <patricio.oliveira@tecsoft.info>
2023-12-22 14:02:35 -0300

    feat - ajustes de nomenclatura, script e coleta de informacoes de login

--- popup.js
+++ popup.js
@@ -2,10 +2,14 @@ import { RequisicoesHTTP } from './js/httpbase.js';
 
 const requisicoes = new RequisicoesHTTP();
 
-let btntest = document.getElementById("button-login");
-if(btntest != null){
-  btntest?.addEventListener("click", async () => {
-    console.log("teste");
+let usuario = document.getElementById("user").v;
+let senha = document.getElementById("password");
+
+let submitButton = document.getElementById("button-login");
+
+if (submitButton != null && usuario != null && senha != null) {
+  submitButton?.addEventListener("click", async () => {
+    console.log(usuario + " - " + senha);
     var response = await requisicoes.get('/posts/1');
     console.log(response);
 
--- script.js
+++ script.js
@@ -4,8 +4,6 @@
     window.XMLHttpRequest.prototype.open = function() {
         console.log('Método: ' + arguments[0]); // Método da solicitação (GET, POST, etc.)
         console.log('URL: ' + arguments[1]); // URL da solicitação
-        console.log('Cabeçalhos: ', this.getAllResponseHeaders()); // Todos os cabeçalhos da resposta
-        console.log('Passou aqui!')
 
         // Adicione um ouvinte para capturar o corpo da resposta
         this.addEventListener('load', function() {
